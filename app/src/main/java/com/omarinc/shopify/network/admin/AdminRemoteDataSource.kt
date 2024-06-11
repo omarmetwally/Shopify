5d887a2d4d769e6dedca968ef1a8ec0de6d4a0eb
walaa <wlashban99@gmail.com>
2024-06-11 20:42:46 +0300

    draft order implementation

--- app/src/main/java/com/omarinc/shopify/network/admin/AdminRemoteDataSource.kt
+++ app/src/main/java/com/omarinc/shopify/network/admin/AdminRemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.omarinc.shopify.network.admin
 
 import com.omarinc.shopify.models.DiscountCodesResponse
+import com.omarinc.shopify.models.DraftOrderRequest
+import com.omarinc.shopify.models.DraftOrderResponse
 import com.omarinc.shopify.models.PriceRulesResponse
 import com.omarinc.shopify.network.ApiState
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +11,12 @@ interface AdminRemoteDataSource {
 
     suspend fun getCoupons(): Flow<ApiState<PriceRulesResponse>>
     suspend fun getCouponDetails(couponId:String): Flow<ApiState<DiscountCodesResponse>>
+
+    suspend fun createDraftOrder(draftOrder: DraftOrderRequest): Flow<ApiState<DraftOrderResponse>>
+
+    suspend fun completeDraftOrder(orderId:Long): Flow<ApiState<DraftOrderResponse>>
+
+    suspend fun sendInvoice(orderId:Long): Flow<ApiState<DraftOrderResponse>>
+
+
 }
