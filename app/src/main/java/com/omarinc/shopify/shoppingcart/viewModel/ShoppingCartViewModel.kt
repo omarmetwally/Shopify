6844597ec2240f14f73336a55614f55f5e481291
Hadi_Abdelwhab <105137729+HadiAbdelwhab@users.noreply.github.com>
2024-06-22 15:10:32 +0300

    currency is working with shopping cart

--- app/src/main/java/com/omarinc/shopify/shoppingcart/viewModel/ShoppingCartViewModel.kt
+++ app/src/main/java/com/omarinc/shopify/shoppingcart/viewModel/ShoppingCartViewModel.kt
@@ -1,14 +1,21 @@
 package com.omarinc.shopify.shoppingcart.viewModel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.omarinc.shopify.home.viewmodel.HomeViewModel.Companion.TAG
 import com.omarinc.shopify.model.ShopifyRepository
 import com.omarinc.shopify.models.CartProduct
 import com.omarinc.shopify.models.CheckoutResponse
+import com.omarinc.shopify.models.CurrencyResponse
 import com.omarinc.shopify.network.ApiState
 import com.omarinc.shopify.type.CheckoutLineItemInput
+import com.omarinc.shopify.utilities.Constants
 import com.omarinc.shopify.utilities.Constants.USER_EMAIL
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
 class ShoppingCartViewModel(private val repository: ShopifyRepository) : ViewModel() {
@@ -21,6 +28,14 @@ class ShoppingCartViewModel(private val repository: ShopifyRepository) : ViewMod
 
     private val _checkoutResponse = MutableStateFlow<ApiState<CheckoutResponse?>>(ApiState.Loading)
     val checkoutResponse: MutableStateFlow<ApiState<CheckoutResponse?>> = _checkoutResponse
+
+    private val _currencyUnit = MutableStateFlow<String>("EGP")
+    val currencyUnit = _currencyUnit.asStateFlow()
+
+    private var _requiredCurrency = MutableStateFlow<ApiState<CurrencyResponse>>(ApiState.Loading)
+    val requiredCurrency = _requiredCurrency.asStateFlow()
+
+
 
     fun getShoppingCartItems(cartId: String) {
 
@@ -58,5 +73,28 @@ class ShoppingCartViewModel(private val repository: ShopifyRepository) : ViewMod
 
         }
     }
+
+
+    fun getCurrencyUnit() {
+
+        viewModelScope.launch(Dispatchers.IO) {
+            _currencyUnit.value = repository.readCurrencyUnit(Constants.CURRENCY_UNIT)
+        }
+    }
+    fun getRequiredCurrency() {
+        Log.i(TAG, "getRequiredCurrency: ")
+        viewModelScope.launch(Dispatchers.IO) {
+
+            repository.getCurrencyRate(repository.readCurrencyUnit(Constants.CURRENCY_UNIT))
+                .catch { error ->
+                    _requiredCurrency.value = ApiState.Failure(error)
+                }
+                .collect { response ->
+                    _requiredCurrency.value =
+                        response ?: ApiState.Failure(Throwable("Something went wrong"))
+                }
+        }
+    }
+
 
 }
