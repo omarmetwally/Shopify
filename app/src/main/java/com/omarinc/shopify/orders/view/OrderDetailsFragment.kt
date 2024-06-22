e6319e44bd8a072abf4d1c031af18bb1eaca9541
walaa <wlashban99@gmail.com>
2024-06-22 21:09:08 +0300

    add back button , handle view pager

--- app/src/main/java/com/omarinc/shopify/orders/view/OrderDetailsFragment.kt
+++ app/src/main/java/com/omarinc/shopify/orders/view/OrderDetailsFragment.kt
@@ -44,7 +44,9 @@ class OrderDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        binding.btnBack.setOnClickListener{
+            findNavController().navigateUp()
+        }
         setUpProductsAdapter()
         collectOrderDetails()
     }
